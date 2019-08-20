610bec81c1b2bd740a9d86747fa2ee68db550acc
PierrickD <pierrick.durrmeyer@gmail.com>
2019-08-20 11:44:15 +0200

    Add req : -security-csrf -validator

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Participants;
+use App\Form\FormulaireAjouterProfilType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\validator\Constraints as Assert;
 
 class ProfilController extends AbstractController
 {
@@ -13,4 +17,21 @@ class ProfilController extends AbstractController
             'controller_name' => 'ProfilController',
         ]);
     }
+    public function nouveauProfil(Request $request)
+    {
+        $participants = new Participants();
+        $nouveauProfil = $this->createForm(FormulaireAjouterProfilType::class, $participants);
+
+        $nouveauProfil->handleRequest($request);
+        if($nouveauProfil->isSubmitted()){
+            //blabla
+
+            return $this->redirectToRoute("Profil");
+        }
+        return $this->render('profil/nouveauProfil.html.twig', [
+            'controller_name' => 'ProfilController',
+            'nouveauProfil' => $nouveauProfil->createView()
+        ]);
+    }
+
 }
