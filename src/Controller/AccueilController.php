5bd7b76869b56dd31241d6936c5972b651bad339
PierrickD <pierrick.durrmeyer@gmail.com>
2019-08-27 11:57:54 +0200

    assemblage filtre

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,25 +5,110 @@ namespace App\Controller;
 use App\Entity\Participants;
 use App\Entity\Sites;
 use App\Entity\Sorties;
+use Doctrine\Common\Collections\Criteria;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Etats;
 
 class AccueilController extends AbstractController
 {
-    public function accueil()
+    public function accueil(Request $request)
     {
+//        $listAdverts = $repository->findBy(
+//            array('author' => 'Alexandre'), // Critere
+//            array('date' => 'desc'),        // Tri
+//            5,                              // Limite
+//            0                               // Offset
+//        );
         $repositorySortie = $this->getDoctrine()->getManager()->getRepository(Sorties::class);
-//        $sorties = $repositorySortie->findBy(["etatsNoEtat" => Etats::Ouverte]);
+//        $sorties = $repositorySortie->findBy(["etatsNoEtat" => Etats::Ouverte,"etatsNoEtat" => Etats::Cloturee, "etatsNoEtat" => Etats::Activite_en_cours]);
         $sorties = $repositorySortie->findAll();
         $repositorySites = $this->getDoctrine()->getManager()->getRepository(Sites::class);
         $Sites = $repositorySites->findAll();
 
-//        $allEtat = array();
+
+
+        if($request->isMethod('post')) {
+
+            $expression=null;
+            $expressionBuilder = Criteria::expr();
+
+
+//            $posts = $request->request->all();
+            $formRecherche = $request->request->get("recherche");
+            $formDateMin = $request->request->get("dateMin");
+            $formDateMax = $request->request->get("dateMax");
+            $formSite = $request->request->get("site");
+            $formOrganisateur = $request->get("organisateur");
+            $formInscrit = $request->request->get("inscrit");
+            $formPasInscrit = $request->request->get("pasInscrit");
+            $formPasser = $request->request->get("passer");
+
+
+            $filtre = array();
+            if ($formRecherche != "") {
+//                $filtre[]=$criteria->expr()->contains('nom', $formRecherche);
+                $filtre[] = $expressionBuilder->contains('nom', $formRecherche);
+
+            }
+            if ($formDateMin != "") {
+                $filtre[] = $expressionBuilder->gte('datedebut', $formDateMin);
+            }
+            if ($formDateMax != "") {
+                $filtre[] = $expressionBuilder->lte('datedebut', $formDateMax);
+            }
+            if ($formSite != "") {
+
+                $filtre[] = $expressionBuilder->eq('sorties_no_sortie', $formDateMax);
+            }
+            $usr = $this->get('security.token_storage')->getToken()->getUser();
+
+            if ($formOrganisateur != "") {
+                $filtre[] = $expressionBuilder->eq('organisateur', $usr);
+            } else {
+                $filtre[] = $expressionBuilder->neq('organisateur', $usr);
+            }
+
+            if ($formInscrit != "") {
+//                $filtre[] = $expressionBuilder->eq('organisateur', $usr);
+//            } else {
+//                $filtre[] = $expressionBuilder->neq('organisateur', $usr);
+            }
+
+            if ($formPasInscrit != "") {
+//                $filtre[] = $expressionBuilder->eq('organisateur', $usr);
+//            } else {
+//                $filtre[] = $expressionBuilder->neq('organisateur', $usr);
+            }
+
+            if ($formPasser!=""){
+                $filtre[] = $expressionBuilder->eq('etats_no_etat', Etats::Passee);
+            }
+
+
+//            $em = $this->getEntityManager();
+//            $qb = $em->createQueryBuilder();
 //
-//        foreach($Etats as $etat){
-//            $allEtat[$etat->getNoEtat()] = $etat->getLibelle();
-//        }
+//            $q  = $qb->select(array('p'))
+//                ->from('YourProductBundle:Product', 'p')
+//                ->where(
+//                    $qb->expr()->gt('p.price', $price)
+//                )
+//                ->orderBy('p.price', 'DESC')
+//                ->getQuery();
+            foreach ($filtre as $fil){
+                $expression=$expressionBuilder->andX($expression,$fil);
+            }
+
+            $sorties=$repositorySortie->matching(new Criteria($expression));
+
+            return $this->render('accueil/accueil.html.twig', [
+                'controller_name' => 'AccueilController',
+                'Sorties' => $sorties,
+                'Sites' => $Sites,
+            ]);
+        }
 
         return $this->render('accueil/accueil.html.twig', [
             'controller_name' => 'AccueilController',
